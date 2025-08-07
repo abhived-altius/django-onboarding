d32848acd038b240bb7a666c1ec3d8154f925103
Abhived <abhived.m@altiushub.com>
2025-08-07 15:53:38 +0530

    Add CustomUser model, initial migration, and clean unused import

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,3 +1,41 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 from django.db import models
+from locations.models import Country
 
 # Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None,**extra_fields):
+        if not email:
+            raise ValueError("The E-mail field must be set")
+        
+        user = self.model(email=self.normalize_email(email),**extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff",True)
+        extra_fields.setdefault("is_superuser",True)
+        
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+        
+        return self.create_user(email,password,**extra_fields)
+
+class CustomUser(AbstractBaseUser,PermissionsMixin):
+    email = models.EmailField(unique=True)
+    country = models.ForeignKey(Country, on_delete=models.SET_NULL,null=True)
+    
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    
+    objects = CustomUserManager()
+    
+    USERNAME_FIELD = "email"
+    
+    def __str__(self):
+        return self.email
+    
